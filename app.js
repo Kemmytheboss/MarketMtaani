1e46164f56a84804b6ccca81608aaa9fb8952635
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-22 18:15:41 +0300

    function to fetch data from json-server  created

---
+++ app.js
@@ -0,0 +1,15 @@
+const productList = document.getElementById('product-list');
+const searchInput = document.getElementById('search');
+const sortBtn = document.getElementById('sortBtn');
+const darkModeToggle = document.getElementById('darkMode');
+
+let products = [];
+let sortedASC = true;
+
+// fetch data from json-server
+
+async function fetchProducts() {
+    const res = await fetch('http://localhost:3000/products');
+    products = await res.json();
+    renderProducts (products);
+}
